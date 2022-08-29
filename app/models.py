53ef48ac4935b0d8c3753cc8d23df0576e8b0e65
eldar-elbrus <elbrus56@mail.ru>
2022-08-29 16:12:12 +0500

    Добавлена авторизация в админке

--- app/models.py
+++ app/models.py
@@ -23,20 +23,15 @@ class Post(db.Model):
     # Если будут скобки у datetime.now(), то дефолтное значение будет задаваться в момент запуска программы и будет для новых постов одним и тем же. Если же убрать скобки, то функция передастся как параметр, и будет вызываться каждый раз при добавлении нового поста
     created = db.Column(db.DateTime, default=datetime.now)
 
-
     def __init__(self, *args, **kwargs):
         super(Post, self).__init__(*args,**kwargs)
         self.generate_slug()
 
-
     tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
 
-
-    
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
-
 
     def __repr__(self):
         return f'<Post id: {self.id}, title: {self.title}>'
@@ -99,3 +94,23 @@ class Role(db.Model, RoleMixin):
 # >>> from models import Post
 # >>> post = Post.query.all()
 # >>> post
+
+
+# >>> from app import db
+# >>> from models import User
+# >>> from app import user_datastore
+# >>> user = User.query.first()
+# >>> user
+# <User 1>
+# >>> user.email
+# 'test@test.ru'
+# >>> user_datastore.create_role(name='admin', description='administrator')
+# db.session.commit()
+
+# >>> from models import Role
+# >>> role = Role.query.first()
+# >>> role
+# <Role 1>
+# >>> user_datastore.add_role_to_user(user, role)
+# True
+# >>> db.session.commit()
